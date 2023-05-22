32c724a8622706eeb778963ef4d8f213e25902ac
Yifan Zhu <84437098+hala64@users.noreply.github.com>
2023-05-22 13:23:58 +0800

    Update generate_data.py

--- generate_poisoned_dataset/generate_data.py
+++ generate_poisoned_dataset/generate_data.py
@@ -1,35 +1,34 @@
 from .class_wise import classwise
-from .error_min import errmin
-from .args import get_args
+import torch
+import os
 
-full_poison = ['random-c','region4','region16','region64','err-max','err-min-s','err-min-c','ntga','ar',
-          'robustem','advinduce','hyp']
+full_poison = ['random-c','region4','region16','region64', 'err-min-s', 'err-min-c', 'ntga', 'ar',
+          'robustem',' hyp', 'entf']
 
 
 def generate_poison(args, poison):
-    if poison == 'random-c' or 'region4' or 'region16'or 'region64':
-        poison_trainset = classwise(args, poison)
+    if poison == 'random-c' or 'region4' or 'region16' or 'region64':
+        poisons = classwise(args, poison)
     elif poison == 'err-min-s':
-        poison_trainset = errmin(args)
+        # generated from https://github.com/HanxunH/Unlearnable-Examples
+        poisons = torch.load(os.path.join('poisoned_data', 'errmins_poisons.pt'))
     elif poison == 'err-min-c':
-        poison_trainset = errmin(args, cl_wise=True)
-    elif poison == 'err-max':
-        from .error_max import errmax
-        poison_trainset = errmax(args)
+        # generated from https://github.com/HanxunH/Unlearnable-Examples
+        poisons = torch.load(os.path.join('poisoned_data', 'errminc_poisons.pt'))
     elif poison == 'ntga':
-        from .ntga.generate_attack import _ntga
-        poison_trainset = _ntga(args)
+        # generated from https://github.com/lionelmessi6410/ntga
+        poisons = torch.load(os.path.join('poisoned_data', 'ntga_poisons.pt'))
     elif poison == 'robustem':
-        from .robustem.generate_robust_em import robust_em
-        poison_trainset = robust_em(args)
+        # generated from https://github.com/fshp971/robust-unlearnable-examples
+        poisons = torch.load(os.path.join('poisoned_data', 'robustem_poisons.pt'))
     elif poison == 'hyp':
-        from .hypocritical.main import hypocritical
-        poison_trainset = hypocritical()
-    elif poison == 'advinduce':
-        from .adv_inducing import adv_induce
-        poison_trainset = adv_induce(args)
+        # generated from https://github.com/TLMichael/Hypocritical-Perturbation
+        poisons = torch.load(os.path.join('poisoned_data', 'hyp_poisons.pt'))
     elif poison == 'ar':
-        from .ar import autoregressive
-        poison_trainset = autoregressive()
+        # generated from https://github.com/psandovalsegura/autoregressive-poisoning
+        poisons = torch.load(os.path.join('poisoned_data', 'ar_poisons.pt'))
+    elif poison == 'entf':
+        # generated from https://github.com/WenRuiUSTC/EntF
+        poisons = torch.load(os.path.join('poisoned_data', 'entf_poisons.pt'))
 
-    return poison_trainset
+    return poisons
