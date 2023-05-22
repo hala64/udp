ae054aeed8ccb8e195b5b313c625db61f1798de9
Yifan Zhu <84437098+hala64@users.noreply.github.com>
2023-05-22 13:45:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,191 +8,103 @@ import os
 
 from PIL import Image
 from torchvision import transforms
-from generate_poisoned_dataset.TinyImageNet_load import TinyImageNet_load
+from .TinyImageNet_load import TinyImageNet_load
 from torchtoolbox.transform import Cutout
-
-class SortedCIFAR10(torch.utils.data.Dataset):
-    def __init__(self, dataset, num_classes=10):
-
-        self.dataset = dataset
-
-        self.dummy = list([torch.zeros(3, 32, 32), 0] for _ in range(len(self.dataset)))
-
-        self.num_classes = num_classes
-
-        self.accumulator = np.zeros(self.num_classes)
-
-        self.calculator = np.zeros(self.num_classes)
-
-        for i in range(len(self.dataset)):
-            for j in range(self.num_classes):
-                if self.dataset[i][1] == j:
-                    self.accumulator[j] += 1
-
-        for i in range(len(self.dataset)):
-            # print(list(dummytestset[i]))
-            # dummytestset[i] = list(dummytestset[i])
-            for j in range(self.num_classes):
-                # print(k[j])
-                if self.dataset[i][1] == j:
-                    # print(testset[i][0])
-                    # print(dummytestset[i][0])
-                    # print(dummytestset[i][1])
-                    # print(dummytestset[i][0].size())
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][0] = self.dataset[i][0]
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][1] = j
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][0] = self.dataset[i][0]
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][1] = j
-                    self.calculator[j] += 1
-        for i in range(len(self.dataset)):
-            self.dummy[i] = tuple(self.dummy[i])
-
-    def sample(self, label): #with label
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        return self.sortedset
-
-    def image(self, label): #withou label
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        self.image = [self.sortedset[i][0].detach().cpu() for i in range(len(self.sortedset))]
-        return self.image
-
-    def data(self):
-        return self.dummy
-
-    def __getitem__(self,item):
-        return self.dummy[item]
-
-    def __len__(self):
-        return len(self.dummy)
+from torchvision.datasets import CIFAR10, CIFAR100, ImageFolder
 
 
-class SortedCIFAR100(torch.utils.data.Dataset):
-    def __init__(self, dataset, num_classes=100):
+class CIFAR10PoisonIndex(CIFAR10):
+    def __init__(self, delta: torch.FloatTensor = None, pseudo_labels=None, ratio=1.0,
+                 class_wise_pseduo_labels=None, **kwargs):
+        super(CIFAR10PoisonIndex, self).__init__(**kwargs)
+        self.delta = delta
+        if pseudo_labels is not None:
+            self.targets = pseudo_labels.astype(np.compat.long)
+        assert ratio <= 1.0 and ratio > 0
 
-        self.dataset = dataset
+        if self.delta is not None:
+            if len(delta) == 10:
+                if class_wise_pseduo_labels is not None:
+                    class_wise_pseduo_targets = class_wise_pseduo_labels.astype(np.compat.long)
+                    self.delta = self.delta[torch.tensor(class_wise_pseduo_targets)]
+                else:
+                    self.delta = self.delta[torch.tensor(self.targets)]
+            if delta.shape != self.data.shape:
+                self.delta = self.delta.permute(0, 2, 3, 1)
+                assert self.delta.shape == self.data.shape
 
-        self.dummy = list([torch.zeros(3, 32, 32), 0] for _ in range(len(self.dataset)))
+            set_size = int(len(self.data) * ratio)
+            if set_size < len(self.data):
+                self.delta[set_size:] = 0.0
+            self.delta = self.delta.mul(255).cpu().numpy()
+            self.data = np.clip(self.data.astype(np.float32) + self.delta, 0, 255).astype(np.uint8)
 
-        self.num_classes = num_classes
-
-        self.accumulator = np.zeros(self.num_classes)
-
-        self.calculator = np.zeros(self.num_classes)
-
-        for i in range(len(self.dataset)):
-            for j in range(self.num_classes):
-                if self.dataset[i][1] == j:
-                    self.accumulator[j] += 1
-
-        for i in range(len(self.dataset)):
-            # print(list(dummytestset[i]))
-            # dummytestset[i] = list(dummytestset[i])
-            for j in range(self.num_classes):
-                # print(k[j])
-                if self.dataset[i][1] == j:
-                    # print(testset[i][0])
-                    # print(dummytestset[i][0])
-                    # print(dummytestset[i][1])
-                    # print(dummytestset[i][0].size())
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][0] = self.dataset[i][0]
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][1] = j
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][0] = self.dataset[i][0]
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][1] = j
-                    self.calculator[j] += 1
-        for i in range(len(self.dataset)):
-            self.dummy[i] = tuple(self.dummy[i])
-
-    def sample(self, label): #with labels
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        return self.sortedset
-
-    def image(self, label): #without labels
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        self.image = [self.sortedset[i][0].detach().cpu() for i in range(len(self.sortedset))]
-        return self.image
-
-    def data(self):
-        return self.dummy
-
-    def __getitem__(self,item):
-        return self.dummy[item]
-
-    def __len__(self):
-        return len(self.dummy)
-
-class SortedTinyImageNet():
-    def __init__(self, dataset, num_classes=200):
-
-        self.dataset = dataset
-
-        self.dummy = list([torch.zeros(3, 64, 64), 0] for _ in range(len(self.dataset)))
-
-        self.num_classes = num_classes
-
-        self.accumulator = np.zeros(self.num_classes)
-
-        self.calculator = np.zeros(self.num_classes)
-        for j in range(200):
-            self.accumulator[j] = 500
-        '''
-        for i in range(len(self.dataset)):
-            if i % 100 == 0:
-                print(i)
-            for j in range(self.num_classes):
-                if self.dataset[i][1] == j:
-                    self.accumulator[j] += 1
-        '''
+    def __getitem__(self, idx):
+        img, target = self.data[idx], self.targets[idx]
+        img = Image.fromarray(img)
+        img = self.transform(img)
+        return img, target, idx
 
 
-        for i in range(len(self.dataset)):
-            if i % 100 == 0:
-                print(i)
-            # print(list(dummytestset[i]))
-            # dummytestset[i] = list(dummytestset[i])
-            for j in range(self.num_classes):
-                # print(k[j])
-                if self.dataset[i][1] == j:
-                    # print(testset[i][0])
-                    # print(dummytestset[i][0])
-                    # print(dummytestset[i][1])
-                    # print(dummytestset[i][0].size())
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][0] = self.dataset[i][0]
-                    #self.dummy[int(self.calculator[j]) + int(len(self.dataset)/self.num_classes) * j][1] = j
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][0] = self.dataset[i][0]
-                    self.dummy[int(self.calculator[j]) + int(np.sum(self.accumulator[:j]))][1] = j
-                    self.calculator[j] += 1
-        for i in range(len(self.dataset)):
-            self.dummy[i] = tuple(self.dummy[i])
+class CIFAR100PoisonIndex(CIFAR100):
+    def __init__(self, delta: torch.FloatTensor = None, pseudo_labels=None, ratio=1.0,
+                 class_wise_pseduo_labels=None, **kwargs):
+        super(CIFAR100PoisonIndex, self).__init__(**kwargs)
+        self.delta = delta
+        if pseudo_labels is not None:
+            self.targets = pseudo_labels.astype(np.compat.long)
+        assert ratio <= 1.0 and ratio > 0
 
-    def sample(self, label): #with labels
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        return self.sortedset
+        if self.delta is not None:
+            if len(delta) == 100:
+                if class_wise_pseduo_labels is not None:
+                    class_wise_pseduo_targets = class_wise_pseduo_labels.astype(np.compat.long)
+                    self.delta = self.delta[torch.tensor(class_wise_pseduo_targets)]
+                else:
+                    self.delta = self.delta[torch.tensor(self.targets)]
+            if delta.shape != self.data.shape:
+                self.delta = self.delta.permute(0, 2, 3, 1)
+                assert self.delta.shape == self.data.shape
 
-    def image(self, label): #without labels
-        self.sortedset = self.dummy[label * int(len(self.dataset)/self.num_classes):
-                                          (label + 1) * int(len(self.dataset)/self.num_classes)]
-        self.sortedset= tuple(self.sortedset)
-        self.image = [self.sortedset[i][0].detach().cpu() for i in range(len(self.sortedset))]
-        return self.image
+            set_size = int(len(self.data) * ratio)
+            if set_size < len(self.data):
+                self.delta[set_size:] = 0.0
+            self.delta = self.delta.mul(255).cpu().numpy()
+            self.data = np.clip(self.data.astype(np.float32) + self.delta, 0, 255).astype(np.uint8)
 
-    def data(self):
-        return self.dummy
+    def __getitem__(self, idx):
+        img, target = self.data[idx], self.targets[idx]
+        img = Image.fromarray(img)
+        img = self.transform(img)
+        return img, target, idx
 
-    def __getitem__(self,item):
-        return self.dummy[item]
 
-    def __len__(self):
-        return len(self.dummy)
+class TinyImageNetPoisonIndex(ImageFolder):
+    def __init__(self, root, train=True, transform=None, delta: torch.FloatTensor = None):
+        if train:
+            root = os.path.join(root, 'train')
+        else:
+            root = os.path.join(root, 'val')
+        super().__init__(root, transform=transform)
+        self.delta = delta
+
+    def __getitem__(self, index: int):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.delta is not None:
+            if len(self.delta) == 200:
+                delta = self.delta[target]
+            else:
+                delta = self.delta[index]
+            delta = delta.mul(255).numpy().transpose(1, 2, 0)
+            sample = np.asarray(sample)
+            sample = np.clip(sample.astype(np.float32) + delta, 0, 255).astype(np.uint8)
+            sample = Image.fromarray(sample, mode='RGB')
+
+        sample = self.transform(sample)
+        return sample, target, index
+
+
 
 def Normalize(x, mean, std, device):
     x = x.to(device)
@@ -304,7 +216,7 @@ def PGD_attack(model, X, y, device, epsilon=8/255, num_steps=10, step_size=1/255
             x_adv = torch.clamp(x_adv, normal_zero, normal_one)
         else:
             x_adv = torch.min(torch.max(x_adv, X - epsilon), X + epsilon)
-            x_adv = torch.clamp(x_adv, 0.0, 1.0)  # clamp to [0,1]
+            x_adv = torch.clamp(x_adv, 0.0, 1.0)  # clamp to [0,1] without normalize
         optimizer.step()
     #torch.cuda.empty_cache()
     return x_adv
@@ -334,7 +246,7 @@ def CW_attack(model, X, y, device, epsilon=8/255, num_steps=10, step_size=1/255,
             x_adv = torch.clamp(x_adv, normal_zero, normal_one)
         else:
             x_adv = torch.min(torch.max(x_adv, X - epsilon), X + epsilon)
-            x_adv = torch.clamp(x_adv, 0.0, 1.0)  # clamp to [0,1]
+            x_adv = torch.clamp(x_adv, 0.0, 1.0)  # clamp to [0,1] without normalize
     #torch.cuda.empty_cache()
     return x_adv
 
@@ -492,43 +404,43 @@ def make_labels(model, trainset, device, batch_size=100):
 
     return dummy_trainset
 
-def data_utils(dataset,aug=False):
-    if dataset == 'CIFAR-10':
+def data_utils(args):
+    if args.dataset == 'CIFAR-10':
         width = 32
         labels = 10
-        if aug:
+        if args.data_augmentation:
             transform_train = transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
             ])
-            trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True,
+            trainset = CIFAR10PoisonIndex(root='../data', train=True, download=True,
                                                 transform=transform_train)
         else:
-            trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True,
+            trainset = CIFAR10PoisonIndex(root='../data', train=True, download=True,
                                                     transform=transforms.ToTensor())
-        testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True,
+        testset = CIFAR10PoisonIndex(root='../data', train=False, download=True,
                                                 transform=transforms.ToTensor())
-    elif dataset == 'CIFAR-100':
+    elif args.dataset == 'CIFAR-100':
         width = 32
         labels = 100
-        if aug:
+        if args.data_augmentation:
             transform_train = transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
             ])
-            trainset = torchvision.datasets.CIFAR100(root='../data', train=True, download=True,
+            trainset = CIFAR100PoisonIndex(root=args.data, train=True, download=True,
                                                 transform=transform_train)
         else:
-            trainset = torchvision.datasets.CIFAR100(root='../data', train=False, download=True,
+            trainset = CIFAR100PoisonIndex(root=args.data, train=False, download=True,
                                                     transform=transforms.ToTensor())
-        testset = torchvision.datasets.CIFAR100(root='../data', train=False, download=True,
+        testset = CIFAR100PoisonIndex(root=args.data, train=False, download=True,
                                                 transform=transforms.ToTensor())
-    elif dataset == 'TinyImageNet':
+    elif args.dataset == 'TinyImageNet':
         width = 64
         labels = 200
-        if aug:
+        if args.data_augmentation:
             transform_train = transforms.Compose([
                 transforms.RandomResizedCrop(64),
                 transforms.RandomHorizontalFlip(),
@@ -536,12 +448,12 @@ def data_utils(dataset,aug=False):
                 Cutout(),
                 transforms.ToTensor(),
             ])
-            trainset = TinyImageNet_load(root='../data/tiny-imagenet-200/', train=True,
+            trainset = TinyImageNetPoisonIndex(root='../data/tiny-imagenet-200/', train=True,
                                         transform=transform_train)
         else:
-            trainset = TinyImageNet_load(root='../data/tiny-imagenet-200/', train=True,
+            trainset = TinyImageNetPoisonIndex(root='../data/tiny-imagenet-200/', train=True,
                                         transform=transforms.ToTensor())
-        testset = TinyImageNet_load(root='../data/tiny-imagenet-200/', train=False,
+        testset = TinyImageNetPoisonIndex(root='../data/tiny-imagenet-200/', train=False,
                                         transform=transforms.ToTensor())
     else:
         raise {'dataset error'}
@@ -549,28 +461,47 @@ def data_utils(dataset,aug=False):
     return width, labels, trainset, testset
 
 def poison_aug(args, poison_set):
-    if args.dataset == 'CIFAR-10' or 'CIFAR-100':
-        transform_poison = transforms.Compose([
-            transforms.ToPILImage(),
+    if args.dataset == 'CIFAR-10':
+        transform = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
         ])
+        clean_set = torchvision.datasets.CIFAR10(root=args.data, train=True, download=True, transform=transforms.ToTensor())
+        poison = torch.zeros(len(poison_set), 3, 32, 32)
+        assert len(clean_set) == len(poison)
+        for i in range(len(poison)):
+            poison[i] = poison_set[i][0] - clean_set[i][0]
+        poison_set = CIFAR10PoisonIndex(root=args.data, train=True, download=True, transform=transform, delta=poison)
+    if args.dataset == 'CIFAR-100':
+        transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+        clean_set = torchvision.datasets.CIFAR100(root=args.data, train=True, download=True, transform=transforms.ToTensor())
+        poison = torch.zeros(len(poison_set), 3, 32, 32)
+        assert len(clean_set) == len(poison)
+        for i in range(len(poison)):
+            poison[i] = poison_set[i][0] - clean_set[i][0]
+        poison_set = CIFAR100PoisonIndex(root=args.data, train=True, download=True, transform=transform, delta=poison)
     elif args.dataset == 'TinyImageNet':
-        transform_poison = transforms.Compose([
-            transforms.ToPILImage(),
+        transform = transforms.Compose([
             transforms.RandomResizedCrop(64),
             transforms.RandomHorizontalFlip(),
             transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
             Cutout(),
             transforms.ToTensor(),
         ])
+        clean_set = TinyImageNet_load(root='../data/tiny-imagenet-200/', train=True,
+                                     transform=transforms.ToTensor())
+        poison = torch.zeros(len(poison_set), 3, 64, 64)
+        assert len(clean_set) == len(poison)
+        for i in range(len(poison)):
+            poison[i] = poison_set[i][0] - clean_set[i][0]
+        poison_set = TinyImageNetPoisonIndex(root='../data/tiny-imagenet-200/', train=True, transform=transform, delta=poison)
     else:
         raise {'dataset error'}
-
-    for i in range(len(poison_set)):
-        # poison_trainset[i][0] = (poison_trainset[i][0] * 255).type(torch.uint8)
-        poison_set[i][0] = transform_poison(poison_set[i][0])
 
     return poison_set
 
