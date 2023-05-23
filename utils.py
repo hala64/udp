abc6d58852443119cba441d093ace22f17ea4f43
Yifan Zhu <84437098+hala64@users.noreply.github.com>
2023-05-23 12:34:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -418,30 +418,3 @@ def data_utils(args):
     return width, labels, trainset, testset
 
 
-class AdversarialPoison(torch.utils.data.Dataset):
-    def __init__(self, root, baseset):
-        self.baseset = baseset
-        self.transform = self.baseset.transform
-        self.samples = os.listdir(os.path.join(root, 'data'))
-        self.root = root
-
-        # Load images into memory to prevent IO from disk
-        self.data, self.targets = self._load_images()
-
-    def __len__(self):
-        return len(self.baseset)
-
-    def __getitem__(self, idx):
-        return self.transform(self.data[idx]), self.targets[idx]
-
-    def _load_images(self):
-        data = list(torch.zeros(3,32,32) for _ in range(len(self.baseset)))
-        targets = list(0 for _ in range(len(self.baseset)))
-        num_data_to_load = len(self.baseset)
-        for i in range(num_data_to_load):
-            true_index = int(self.samples[i].split('.')[0])
-            _, label = self.baseset[true_index]
-            data[true_index]= (Image.open(os.path.join(self.root, 'data', self.samples[i])).copy())
-            targets[true_index] = (label)
-        return data, targets
-
